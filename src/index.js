2bfed94f0d29fce962de90c32a5eb787cd9cb738
mk-goyal <mkgoyal@iitk.ac.in>
2021-05-19 02:27:14 +0530

    QuizTime

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/style.css';
+import quizService from './quizService';
+import QuestionBox from './components/QuestionBox'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class QuizTime extends Component {
+  state = {
+    questionBank: [],
+    scores:0,
+    responses:0
+  };
+  getQuestions = () => {
+    quizService().then(question => {
+      this.setState({
+        questionBank: question
+      });
+    });
+  };
+  computeAnswer = (answer, correctAnswer) => {
+    if(answer = correctAnswer){
+        this.setState({
+          score: this.state.score + 1
+        })
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1:5
+    })
+  }
+  componentDidMount() {
+    this.getQuestions();
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div className='title'>QuizTime</div>
+        {this.state.questionBank.length > 0 && this.state.responses < 5 && this.state.questionBank.map(
+          ({question, answers, correct, questionID}) => (
+            <QuestionBox 
+            question={question} 
+            options={answers} 
+            key={questionID}
+            selected={answer => this.computeAnswer(answer, correct)}/>
+          )
+          )}
+          {this.state.responses === 5 ? (<h2>{this.state.score}</h2>): null };
+      </div>
+    );
+  };
+}
+ReactDOM.render(<QuizTime />,document.getElementById("root"));
